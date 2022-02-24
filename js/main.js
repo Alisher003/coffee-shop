d5f7e26050855b4a3e9cf1f2548b1ea3b5ba952e
Alisher003 <abduvaliyevalisher003@gmail.com>
2022-02-24 22:06:05 +0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 let navbar = document.querySelector('.navbar');
+let cartItem = document.querySelector('.cart__items__container')
+let searchForm = document.querySelector('.search-form')
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
+}
+
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+}
+
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
 }
